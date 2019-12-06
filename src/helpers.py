b1b4771822419803f2d31e2e188abb1bfefc2bda
celbirlik <celbirlik@gmail.com>
2019-12-06 03:53:06 +0300

    cmatrix

--- src/helpers.py
+++ src/helpers.py
@@ -1,15 +1,16 @@
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 
-def confusion_matrix(y_predicts,y_true,labels,model_name)
-    y_pred = np.argmax(y_vecs, axis=1)
+
+def cf_matrix(y_predicts, y_true, labels, model_name, save=False):
+    y_pred = np.argmax(y_predicts, axis=1)
+    y_true = np.argmax(y_true, axis=1)
     cm = confusion_matrix(y_true, y_pred)
 
     min_val, max_val = 0, 15
 
-    fig, ax = plt.subplots(figsize=(15, 15))
+    fig, ax = plt.subplots(figsize=(24, 24))
     ax.matshow(cm, cmap=plt.cm.Blues)
-    # ax.matshow(cm, cmap=plt.cm.magma_r)
     ax.xaxis.set_ticklabels(labels)
     ax.yaxis.set_ticklabels(labels)
 
@@ -18,12 +19,11 @@ def confusion_matrix(y_predicts,y_true,labels,model_name)
             c = cm[j, i]
             ax.text(i, j, str(c), va='center', ha='center')
 
-
     plt.xticks(range(10))
     plt.yticks(range(10))
-    plt.suptitle('Confusion matrix', size=32)
-    plt.xlabel('True labeling', size=32)
-    plt.ylabel('Predicted labeling', size=32)
-    plt.rcParams.update({'font.size': 28})
-    plt.savefig('cmatrix_'+model_name'+.png')
-
+    plt.suptitle('Confusion matrix for '+model_name, size=24)
+    plt.xlabel('True labeling', size=24)
+    plt.ylabel('Predicted labeling', size=24)
+    plt.rcParams.update({'font.size': 11})
+    if save:
+        plt.savefig('cmatrix_'+model_name+'.png')
